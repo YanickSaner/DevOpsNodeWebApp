e48c0896a520e81561af20551e6d365d83f12307
Yanick Saner <saneryan@students.zhaw.ch>
2025-05-24 23:26:22 +0200

    server.js angepasst für docker

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Render aktiviert von Yanick!!!');
+  res.send('Die letzten Tage von meinem Surface...');
 });
 
 app.listen(PORT, HOST);
