e64e0a68bb8786afef01c3729a825f6060c5f40d
Yanick Saner <saneryan@students.zhaw.ch>
2025-05-25 19:51:13 +0200

    Server.js angepasst

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Bald ist es geschafft!!!');
+  res.send('Jetzt ist es geschafft!!!');
 });
 
 app.listen(PORT, HOST);
